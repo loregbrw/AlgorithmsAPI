480289d6df3b62afb5ba774194126563950c8300
loregbrw <loregobara@gmail.com>
2025-08-11 10:56:53 -0300

    initial commit

---
+++ algorithms-api/src/main.rs
@@ -0,0 +1,14 @@
+use axum::{Router, routing::get};
+
+const BIND_ADDR: &str = "127.0.0.1:2525";
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind(BIND_ADDR).await.unwrap();
+
+    println!("Listening on http://{BIND_ADDR}");
+
+    axum::serve(listener, app).await.unwrap();
+}
